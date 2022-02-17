ba552485fcf3a98f83f41e885eed3b23a750a7d5
Noot-dev <zhangyian1025@outlook.com>
2022-02-17 10:22:49 -0800

    release

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",'./public/index.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Inter', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
